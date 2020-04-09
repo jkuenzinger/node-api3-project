2340254dee4ff0b2f6cf6744d05e6ce98f1ab4e4
jkuenzinger <jkuenzinger228@gmail.com>
2020-04-09 11:43:40 -0700

    added the process.env so that i can host on heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // below we are bringin in our server .js and setting up the server listen so when we run the server it knows which port we want it to run on
 const server = require('./server.js');
 
-server.listen(4000, () => {
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
     console.log('this server is running on port 4000')
 })
